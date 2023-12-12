f71f4b48baf125288fe368e6325a71a6dc2b8483
briangoodell <logins.briandalegoodell@gmail.com>
2023-12-11 22:13:33 -0600

    Testing alphanumeric cracking

--- cracker-gpu.cu
+++ cracker-gpu.cu
@@ -24,11 +24,12 @@
 //                          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 //                          '0','1','2','3','4','5','6','7','8','9'}
 
-// __device__ alphabet[] = {'a'-'a','b'-'a','c'-'a','d'-'a','e'-'a','f'-'a','g'-'a','h'-'a','i'-'a','j'-'a','k'-'a','l'-'a','m'-'a','n'-'a','o'-'a','p'-'a','q'-'a','r'-'a','s'-'a','t'-'a','u'-'a','v'-'a','w'-'a','x'-'a','y'-'a','z'-'a',
-//                          'A'-'a','B'-'a','C'-'a','D'-'a','E'-'a','F'-'a','G'-'a','H'-'a','I'-'a','J'-'a','K'-'a','L'-'a','M'-'a','N'-'a','O'-'a','P'-'a','Q'-'a','R'-'a','S'-'a','T'-'a','U'-'a','V'-'a','W'-'a','X'-'a','Y'-'a','Z'-'a',
-//                          '0'-'a','1'-'a','2'-'a','3'-'a','4'-'a','5'-'a','6'-'a','7'-'a','8'-'a','9'-'a'}
+// __device__ alphabet[] = {'a'-'0','b'-'0','c'-'0','d'-'0','e'-'0','f'-'0','g'-'0','h'-'0','i'-'0','j'-'0','k'-'0','l'-'0','m'-'0','n'-'0','o'-'0','p'-'0','q'-'0','r'-'0','s'-'0','t'-'0','u'-'0','v'-'0','w'-'0','x'-'0','y'-'0','z'-'0',
+//                          '0'-'0','B'-'0','C'-'0','D'-'0','E'-'0','F'-'0','G'-'0','H'-'0','I'-'0','J'-'0','K'-'0','L'-'0','M'-'0','N'-'0','O'-'0','P'-'0','Q'-'0','R'-'0','S'-'0','T'-'0','U'-'0','V'-'0','W'-'0','X'-'0','Y'-'0','Z'-'0',
+//                          '0'-'0','1'-'0','2'-'0','3'-'0','4'-'0','5'-'0','6'-'0','7'-'0','8'-'0','9'-'0'}
 
-__device__ int alphabet[] = {'a'-'a','b'-'a','c'-'a','d'-'a','e'-'a','f'-'a','g'-'a','h'-'a','i'-'a','j'-'a','k'-'a','l'-'a','m'-'a','n'-'a','o'-'a','p'-'a','q'-'a','r'-'a','s'-'a','t'-'a','u'-'a','v'-'a','w'-'a','x'-'a','y'-'a','z'-'a'};
+__device__ int alphabet[] = {'0'-'0','1'-'0','2'-'0','3'-'0','4'-'0','5'-'0','6'-'0','7'-'0','8'-'0','9'-'0',
+                              'a'-'0','b'-'0','c'-'0','d'-'0','e'-'0','f'-'0','g'-'0','h'-'0','i'-'0','j'-'0','k'-'0','l'-'0','m'-'0','n'-'0','o'-'0','p'-'0','q'-'0','r'-'0','s'-'0','t'-'0','u'-'0','v'-'0','w'-'0','x'-'0','y'-'0','z'-'0'};
 
 // Each board will be a block
 #define THREADS_PER_BLOCK (ALPHABET_SIZE * ALPHABET_SIZE)
@@ -311,7 +312,7 @@ __global__ void cracker_thread(password_set_node_t* passwords){
   uint8_t candidate_hash[MD5_DIGEST_LENGTH]; //< This will hold the hash of the candidate password
 
   // Same as the individual thread, but now we have each start on offset, and inc by the # of threads
-  char candidate_passwd[] = "aaaaaaa";
+  char candidate_passwd[] = "0000000";
   candidate_passwd[0]+= alphabet[offsetGPU];
   candidate_passwd[2]+= alphabet[threadIdx.x];
   candidate_passwd[3]+= alphabet[threadIdx.y];
@@ -331,7 +332,7 @@ __global__ void cracker_thread(password_set_node_t* passwords){
   int hash_index;
 
   for(int i = 0; i < ALPHABET_SIZE; i++){
-    while(candidate_passwd[0] < 'a' + alphabet[ALPHABET_SIZE - 1]){
+    while(candidate_passwd[0] < '0' + alphabet[ALPHABET_SIZE - 1]){
       md5String(candidate_passwd, PASSWORD_LENGTH, candidate_hash);
 
       // Get the bucket corresponding to the hash
@@ -354,8 +355,8 @@ __global__ void cracker_thread(password_set_node_t* passwords){
       // }
       candidate_passwd[0] += numUsersGPU;
     }
-    candidate_passwd[0] = 'a' + alphabet[offsetGPU];
-    candidate_passwd[1] = 'a' + alphabet[(candidate_passwd[1] - 'a') + 1];
+    candidate_passwd[0] = '0' + alphabet[offsetGPU];
+    candidate_passwd[1] = '0' + alphabet[(candidate_passwd[1] - '0') + 1];
     // candidate_passwd[0] = 'a' + offsetGPU;
     // candidate_passwd[1]++;
   }
